effee61fb30968d917d65c74dede483865fb9d3a
Dimitrios Metozis <dimitrios.dev@gmail.com>
2018-07-20 15:50:57 +0200

    Initial commit.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DataRecovery
 {
@@ -10,6 +6,8 @@ namespace DataRecovery
     {
         static void Main(string[] args)
         {
+            Console.Write("New Line");
+            Console.ReadLine();
         }
     }
 }
